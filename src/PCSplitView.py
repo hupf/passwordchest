bbba0709c7d50d176f205245d93ed6e8f63d21df
Mathis Hofer <mathis@fsfe.org>
2009-12-26 00:36:43 +0100

    bugfix: splitview resizing constraint

--- src/PCSplitView.py
+++ src/PCSplitView.py
@@ -9,10 +9,22 @@
 from Foundation import *
 from AppKit import *
 
+INFO_WIDTH = 323
+
+
 class PCSplitView(NSSplitView):
+    def drawRect_(self, rect):
+        NSColor.controlHighlightColor().set()
+        NSRectFill(rect)
+        
+        return NSSplitView.drawRect_(self, rect)
+    
     def adjustSubviews(self):
         leftFrameSize = self.subviews()[0].frame().size
         rightFrameSize = self.subviews()[1].frame().size
+        
+        # Right frame should not be smaller than info box
+        rightFrameSize.width = max(rightFrameSize.width, INFO_WIDTH)
         
         self.subviews()[0].setFrameSize_(NSSize(
             self.frame().size.width - rightFrameSize.width,
@@ -22,10 +34,9 @@ class PCSplitView(NSSplitView):
             rightFrameSize.width,
             self.frame().size.height))
         
-        NSSplitView.adjustSubviews(self)
+        return NSSplitView.adjustSubviews(self)
 
-    def drawRect_(self, rect):
-        NSColor.controlHighlightColor().set()
-        NSRectFill(rect)
-        
-        NSSplitView.drawRect_(self, rect)
+
+class PCSplitViewDelegate(NSObject):
+    def splitView_constrainMaxCoordinate_ofSubviewAt_(self, splitView, proposedMaximumPosition, dividerIndex):
+        return min(proposedMaximumPosition, splitView.frame().size.width - INFO_WIDTH - 1)
